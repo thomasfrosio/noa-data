e03242da98b6b606180eee5910bdbe64c514dcca
ffyr2w <tomfros@hotmail.fr>
2021-11-05 03:26:06 +0000

    Add phase shifts

--- assets/util.py
+++ assets/util.py
@@ -2,6 +2,7 @@ import os
 import yaml
 import mrcfile
 import numpy as np
+import warnings
 
 
 def set_cwd(filename):
@@ -10,7 +11,12 @@ def set_cwd(filename):
 
 def save_mrc(filename, data):
     with mrcfile.new(filename, overwrite=True) as mrc:
-        mrc.set_data(data.astype(np.float32))
+        if data.dtype == np.complex64 or data.dtype == np.complex128:
+            with warnings.catch_warnings():
+                warnings.filterwarnings("ignore", category=np.ComplexWarning)
+                mrc.set_data(data.astype(np.complex64))
+        else:
+            mrc.set_data(data.astype(np.float32))
 
 
 def save_txt(filename, string):
@@ -73,3 +79,29 @@ def to_scipy_interp_mode(interp, border, value):
         raise RuntimeError
 
     return scipy_interp_order, scipy_border, scipy_value
+
+
+def get_phase_shift(shape, shift):
+    """
+    :param shape:   [x,y] or [x,y,z] shape
+    :param shift:   [x,y] or [x,y,z] shifts
+    :return: Phase shifts for redundant centered FFTs
+    """
+    if np.size(shape) == 2:
+        x, y = (np.arange(shape[0], dtype=np.float32) - shape[0] // 2,
+                np.arange(shape[1], dtype=np.float32) - shape[1] // 2)
+        gx, gy = np.meshgrid(x, y)
+        factors = -2 * np.pi * (shift[0] * gx / shape[0] +
+                                shift[1] * gy / shape[1])
+
+    elif np.size(shape) == 3:
+        x, y, z = (np.reshape(np.arange(shape[0], dtype=np.float32) - shape[0] // 2, (1, 1, -1)),
+                   np.reshape(np.arange(shape[1], dtype=np.float32) - shape[1] // 2, (1, -1, 1)),
+                   np.reshape(np.arange(shape[2], dtype=np.float32) - shape[2] // 2, (-1, 1, 1)))
+        gz, gy, gx = np.meshgrid(z, y, x, indexing='ij')
+        factors = -2 * np.pi * (shift[0] * gx / shape[0] +
+                                shift[1] * gy / shape[1] +
+                                shift[2] * gz / shape[2])
+    else:
+        raise RuntimeError
+    return np.cos(factors) + 1j * np.sin(factors)
