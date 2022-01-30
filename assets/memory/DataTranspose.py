77eecab6f390eaf7ac72f8b37211346acb3a53dd
ffyr2w <tomfros@hotmail.fr>
2022-01-30 17:58:30 +0100

    Update `memory`
    * Update to rightmost 4D shapes and add batches.

--- assets/memory/DataTranspose.py
+++ assets/memory/DataTranspose.py
@@ -6,7 +6,7 @@ def generate_inputs(param):
     # Values starts at 0, up to N, contiguously.
     for entry in param:
         shape = entry['shape']
-        array = np.arange(np.prod(shape), dtype=np.float32).reshape(np.flip(shape))
+        array = np.arange(np.prod(shape), dtype=np.float32).reshape(shape)
         util.save_mrc(entry['path'], array)
     print("\t-- Generated: inputs")
 
@@ -15,19 +15,8 @@ def generate_expected(param):
     # Load input, transpose and save.
     for i in param:
         array = util.load_mrc(param[i]['input'])
-
-        # C++: x=0, y=1, z=2; Numpy: x=2, y=1, z=0
-        cpp_permutation = np.flip(param[i]['permutation'])
-        numpy_permutation = np.zeros(3, dtype=int)
-        for dim in range(3):
-            if cpp_permutation[dim] == 0:
-                numpy_permutation[dim] = 2
-            elif cpp_permutation[dim] == 2:
-                numpy_permutation[dim] = 0
-            else:
-                numpy_permutation[dim] = 1
-
-        transposed = np.transpose(array, numpy_permutation)
+        permutation = param[i]['permutation']
+        transposed = np.transpose(array, permutation)
         util.save_mrc(param[i]['expected'], transposed)
     print("\t-- Generated: expected")
 
