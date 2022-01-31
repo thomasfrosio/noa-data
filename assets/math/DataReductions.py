324541b4544f8d36bf24f3add25834e5c76dfb0f
ffyr2w <tomfros@hotmail.fr>
2022-01-31 21:42:35 +0100

    Update `math`
    * Update to rightmost 4D shapes and add batches.

--- assets/math/DataReductions.py
+++ assets/math/DataReductions.py
@@ -2,49 +2,84 @@ import numpy as np
 from assets import util
 
 
-def generate_stats(param):
+def generate_input(param):
     # Input:
-    batches = param['input']['batches']
-    shape = param['input']['shape']
-    data = np.random.randn(batches, shape[1], shape[0]).astype(np.float32)
+    shape = np.array(param['shape'], dtype=int)
+    data = np.random.randn(shape[0], shape[1], shape[2], shape[3]).astype(np.float32)
     data += np.linspace(0, 3, data.size).reshape(data.shape)
-    util.save_mrc(param['input']['path'], data)
+    data[3, ...] += np.random.rand()
+    data[5, ...] += np.random.randn()
+    data[:, 3, ...] += np.random.randn()
+    data[..., 20:25] += np.random.randn()
+    data[..., 50:53] += np.random.randn()
+    util.save_mrc(param['path'], data)
 
-    # Outputs:
+
+def generate_all(param):
+    data = util.load_mrc(param['input']['path'])
+    results = {'min': float(np.min(data)),
+               'max': float(np.max(data)),
+               'sum': float(np.sum(data.astype(np.double))),
+               'mean': float(np.mean(data.astype(np.double))),
+               'var': float(np.var(data.astype(np.double))),
+               'std': float(np.std(data.astype(np.double)))}
+    util.save_yaml(param['all']['output_path'], results)
+
+
+def generate_batch(param):
+    data = util.load_mrc(param['input']['path'])
     results = dict()
-    for batch in range(batches):
+    for batch in range(data.shape[0]):
         results[batch] = {'min': float(np.min(data[batch, ...])),
                           'max': float(np.max(data[batch, ...])),
-                          'sum': float(np.sum(data[batch, ...])),
-                          'mean': float(np.mean(data[batch, ...])),
-                          'var': float(np.var(data[batch, ...])),
-                          'std': float(np.std(data[batch, ...]))}
-    util.save_yaml(param['output'], results)
-    print("\t-- Generated: input/expected stats")
+                          'sum': float(np.sum(data[batch, ...].astype(np.double))),
+                          'mean': float(np.mean(data[batch, ...].astype(np.double))),
+                          'var': float(np.var(data[batch, ...].astype(np.double))),
+                          'std': float(np.std(data[batch, ...].astype(np.double)))}
+    util.save_yaml(param['batch']['output_path'], results)
 
 
-def generate_reductions(param):
-    # Inputs:
-    batches = param['input']['batches']
-    nb_vectors = param['input']['vectors']
-    elements_per_vector = param['input']['elements']
+def generate_axes(param):
+    for i in range(4):
+        data = util.load_mrc(param['input']['path'])
+        key = 'axis{}'.format(i)
+        output_shape = param[key]['output_shape']
+        util.save_mrc(param[key]['output_min'], np.min(data, axis=i).reshape(output_shape))
+        util.save_mrc(param[key]['output_max'], np.max(data, axis=i).reshape(output_shape))
+        util.save_mrc(param[key]['output_sum'], np.sum(data.astype(np.double), axis=i).reshape(output_shape))
+        util.save_mrc(param[key]['output_mean'], np.mean(data.astype(np.double), axis=i).reshape(output_shape))
+        util.save_mrc(param[key]['output_var'], np.var(data.astype(np.double), axis=i).reshape(output_shape))
+        util.save_mrc(param[key]['output_std'], np.std(data.astype(np.double), axis=i).reshape(output_shape))
 
-    vectors = np.random.randn(batches, nb_vectors, elements_per_vector).astype(np.float32)
-    vectors += np.linspace(0, 3, vectors.size).reshape(vectors.shape)
-    util.save_mrc(param['input']['path'], vectors)
-    weights = np.random.randn(1, nb_vectors, elements_per_vector).astype(np.float32) + 1
-    util.save_mrc(param['input']['path_weights'], weights)
 
-    # Outputs: reduce vectors into one.
-    util.save_mrc(param['sum'], np.sum(vectors, axis=1))
-    util.save_mrc(param['mean'], np.mean(vectors, axis=1))
-    util.save_mrc(param['weighted_mean'], np.sum(vectors * weights, axis=1) / np.sum(weights, axis=1))
-    print("\t-- Generated: input/expected reductions")
+def generate_complex(param):
+    # Input:
+    shape = param['shape']
+    data = np.ones(shape, dtype=np.complex64)
+    data.real += np.random.randn(shape[0], shape[1], shape[2], shape[3]).astype(np.float32)
+    data.imag += np.random.randn(shape[0], shape[1], shape[2], shape[3]).astype(np.float32)
+    data += np.linspace(0, 3, data.size).reshape(data.shape)
+    util.save_mrc(param['input_path'], data)
+
+    results = dict()
+    sum = np.sum(data.astype(np.complex128))
+    results['sum_real'] = float(sum.real)
+    results['sum_imag'] = float(sum.imag)
+    mean = np.mean(data.astype(np.complex128))
+    results['mean_real'] = float(mean.real)
+    results['mean_imag'] = float(mean.imag)
+    results['var'] = float(np.var(data.astype(np.complex128), ddof=1))
+    results['std'] = float(np.std(data.astype(np.complex128), ddof=1))
+    util.save_yaml(param['output_path'], results)
 
 
 if __name__ == '__main__':
     util.set_cwd(__file__)
 
     parameters = util.load_yaml("tests.yaml")
-    generate_stats(parameters['stats'])
-    generate_reductions(parameters['reductions'])
+    generate_input(parameters['reduce_to_stats']['input'])
+    generate_all(parameters['reduce_to_stats'])
+    generate_batch(parameters['reduce_to_stats'])
+    generate_axes(parameters['reduce_to_stats'])
+    generate_complex(parameters['reduce_complex'])
+    print("\t-- Generated: input/expected stats")
