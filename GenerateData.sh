1c2ab90ba12764a6409319865a93c2286a8c899b
Thomas Frosio <frosiot@gmail.com>
2025-04-07 10:25:06 +0100

    Enforce the version of packages

--- GenerateData.sh
+++ GenerateData.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 # Generates data
 # ==============
 
@@ -6,12 +8,12 @@
 # It will generate the test data needed by noa.
 # All existing generated assets will be overwritten.
 
-# Some colours in our life
+# some colours in our life
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
-# Ensure minimum python version:
+# ensure minimum python version
 if python -c 'import sys; sys.exit(sys.version_info[:2] >= (3,8))' > /dev/null ; then
   >&2 echo -e "${RED}ERROR: \"python\" should point to python3.8 or newer${NC}"
   exit 1
@@ -21,7 +23,7 @@ python -m venv env_noa_data
 source env_noa_data/bin/activate
 pip install -e .
 
-# Generate assets:
+# generate assets
 echo -e "${GREEN}noa-tests: generating data - start${NC}"
 find assets -type f -name "Data*.py" -exec echo "noa-tests: running" {} \; -exec python {} \;
 echo -e "${GREEN}noa-tests: generating data - done${NC}"
@@ -31,6 +33,6 @@ NOA_DATA_PATH="$(
   pwd -P
 )" # https://stackoverflow.com/questions/4774054
 
-echo -e "${GREEN}noa-tests: remember to export NOA_DATA_PATH to \"${NOA_DATA_PATH}\"${NC}"
+echo -e "${GREEN}noa-tests: remember to export NOA_DATA_PATH=\"${NOA_DATA_PATH}\"${NC}"
 
 deactivate
--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name='noa-data',
     version='0.1.0',
-    url='https://github.com/ffyr2w/noa-data.git',
+    url='https://github.com/thomasfrosio/noa-data.git',
     author='Thomas Frosio',
     author_email='frosiot@gmail.com',
     description='Assets for the C++ noa library',
     packages=find_packages(),
     install_requires=[
-        'numpy>=1.21.2',
-        'scipy>=1.7.1',
-        'mrcfile>=1.3.0',
-        'pyyaml',
-        'eulerangles>=1.0.1'
+        'numpy==1.21.6',
+        'scipy==1.7.3',
+        'mrcfile==1.3.0',
+        'pyyaml==6.0.2',
+        'eulerangles==1.0.2'
     ]
 )
